8dcaa4925dd29686209f16bf56c0706493f93a7c
Jeff Dyke <jeff.dyke@gmail.com>
2025-01-24 17:56:05 -0500

    update all version references

--- indexer-certs-creator/config/entrypoint.sh
+++ indexer-certs-creator/config/entrypoint.sh
@@ -8,8 +8,8 @@
 ## Variables
 CERT_TOOL=wazuh-certs-tool.sh
 PASSWORD_TOOL=wazuh-passwords-tool.sh
-PACKAGES_URL=https://packages.wazuh.com/4.9/
-PACKAGES_DEV_URL=https://packages-dev.wazuh.com/4.9/
+PACKAGES_URL=https://packages.wazuh.com/4.10.1/
+PACKAGES_DEV_URL=https://packages-dev.wazuh.com/4.10.1/
 
 ## Check if the cert tool exists in S3 buckets
 CERT_TOOL_PACKAGES=$(curl --silent -I $PACKAGES_URL$CERT_TOOL | grep -E "^HTTP" | awk  '{print $2}')
@@ -59,4 +59,3 @@ do
   chown 999:999 "/certificates/${i}.pem"
   chown 999:999 "/certificates/${i}-key.pem"
 done
-
