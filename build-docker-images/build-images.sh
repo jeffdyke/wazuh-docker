cadd85b176c487d3d08275eb746903ebefa59282
Jeff Dyke <jeff.dyke@gmail.com>
2026-01-02 13:13:40 -0500

    update variables for clean upgrade

--- build-docker-images/build-images.sh
+++ build-docker-images/build-images.sh
@@ -1,9 +1,8 @@
-WAZUH_IMAGE_VERSION=4.10.1
+WAZUH_IMAGE_VERSION=4.14.0
 WAZUH_VERSION=$(echo $WAZUH_IMAGE_VERSION | sed -e 's/\.//g')
 WAZUH_TAG_REVISION=1
 WAZUH_CURRENT_VERSION=$(curl --silent https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '["]tag_name["]:' | sed -E 's/.*\"([^\"]+)\".*/\1/' | cut -c 2- | sed -e 's/\.//g')
 IMAGE_VERSION=${WAZUH_IMAGE_VERSION}
-
 # Wazuh package generator
 # Copyright (C) 2023, Wazuh Inc.
 #
@@ -12,7 +11,7 @@ IMAGE_VERSION=${WAZUH_IMAGE_VERSION}
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
 
-WAZUH_IMAGE_VERSION="4.10.1"
+WAZUH_IMAGE_VERSION="4.14.0"
 WAZUH_TAG_REVISION="1"
 WAZUH_DEV_STAGE=""
 FILEBEAT_MODULE_VERSION="0.4"
